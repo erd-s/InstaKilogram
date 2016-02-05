94899b1559cff657d1b3cacf9f7819747c0b83c8
Christopher Erdos <christophererdos@Christophers-MacBook-Pro.local>
2016-02-05 16:59:10 -0600

    set up skeleton of photo feed and created custom cell.

---
+++ InstaKilogram/PhotoFeedCell.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoFeedCell.swift
+//  InstaKilogram
+//
+//  Created by Christopher Erdos on 2/5/16.
+//  Copyright © 2016 JJones. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFeedCell: UITableViewCell {
+
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var photoView: UIImageView!
+    @IBOutlet var geoLocationLabel: UILabel!
+    
+    @IBOutlet var commentsLabel: UILabel!
+    @IBOutlet var likeCountLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+}
---
+++ InstaKilogram/PhotoFeedTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  PhotoFeedTableViewController.swift
+//  InstaKilogram
+//
+//  Created by Christopher Erdos on 2/5/16.
+//  Copyright © 2016 JJones. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFeedTableViewController: UITableViewController {
+    //MARK: Properties
+    
+    
+    //MARK: Outlets
+    
+    
+    //MARK: View Loading
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+ 
+    //MARK: Button Taps
+    @IBAction func onLikeButtonTap(sender: UIButton) {
+    }
+    
+   
+    @IBAction func onCommentButtonTapped(sender: UIButton) {
+    }
+    
+    
+    @IBAction func onForwardButtonTap(sender: AnyObject) {
+    }
+    
+    @IBAction func onDotDotDotButtonTapped(sender: UIButton) {
+    }
+    
+    
+    //MARK: TableView Stuff
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> PhotoFeedCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("pizza", forIndexPath: indexPath) as! PhotoFeedCell
+        
+//        cell.photoView.image = 
+//        cell.commentsLabel.text = 
+//        cell.likeCountLabel.text = 
+        
+        return cell
+    }
+    
+
+  }
