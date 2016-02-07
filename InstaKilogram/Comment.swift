b22c6e74efc27f158264f3f5567809e9a2a45b5f
Christopher Erdos <christophererdos@Christophers-MacBook-Pro.local>
2016-02-06 20:30:14 -0600

    added Post and Comment custom classes

---
+++ InstaKilogram/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  InstaKilogram
+//
+//  Created by Christopher Erdos on 2/6/16.
+//  Copyright © 2016 JJones. All rights reserved.
+//
+
+import UIKit
+
+class Comment: NSDictionary {
+
+}
