0bd64f605435bdef015f1e870a11983bfa82e304
Christopher Erdos <chris.erdos@gmail.com>
2016-02-09 15:07:48 -0600

    successfully fetched photos from FB and loaded them into tableview

--- InstaKilogram/ProfileViewController.swift
+++ InstaKilogram/ProfileViewController.swift
@@ -14,6 +14,7 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     var currentUserData = [FDataSnapshot]()
     var currentUser = Dictionary<String, AnyObject>()
     var userPhotosArray = [UIImage]()
+    var userDefaults = NSUserDefaults.standardUserDefaults()
     
     //MARK: Outlets
     @IBOutlet weak var postNumberLabel: UILabel!
@@ -35,15 +36,14 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     override func viewWillAppear(animated: Bool) {
-            startListeningAndSetCurrentUser()
+        startListeningAndSetCurrentUser()
+        getPhotos()
     }
     
     //MARK: Custom Functions
     func startListeningAndSetCurrentUser () {
-
         FirebaseData.firebaseData.CURRENT_USER_REF.observeEventType(.Value, withBlock: { snapshot in
-            print(snapshot.value.description)
-            
+//            print(snapshot.value.description)
             self.currentUser = snapshot.value as! Dictionary<String, AnyObject>
             self.usernameTitleLabel.text? = self.currentUser["username"]!.uppercaseString
             if (self.currentUser["name"] != nil) {
@@ -55,23 +55,6 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
             else {
                 self.descriptionLabel.text = "Please click 'Edit Profile' to add a description."
                 self.descriptionLabel.textColor = UIColor.lightGrayColor()
-            }
-            if self.currentUser["userPhoto"] == nil {
-                self.userImage.image = UIImage(named: "defaultPhoto")
-            } else {
-                let decodedData = NSData(base64EncodedString: (self.currentUser["photoString"] as? String)!, options: NSDataBase64DecodingOptions())
-                let decodedImage = UIImage(data: decodedData!)
-                self.userImage.image = decodedImage
-            }
-            if (self.currentUser["photos"] != nil) {
-                self.postNumberLabel.text = String(self.currentUser["photos"]!.count)
-                
-                for photoDict in self.currentUser["photos"] as! [Dictionary<String, AnyObject>] {
-                    let photoString = photoDict["photoString"]
-                    let decodedData = NSData(base64EncodedString: (photoString as? String)!, options: NSDataBase64DecodingOptions())
-                    let decodedImage = UIImage(data: decodedData!)
-                    self.userPhotosArray.insert(decodedImage!, atIndex: 0)
-                }
             }
             if self.currentUser["followers"] != nil {
                 self.followersNumberLabel.text = String(self.currentUser["followers"]!.count)
@@ -88,11 +71,26 @@ class ProfileViewController: UIViewController, UITableViewDataSource, UITableVie
             if self.currentUser["userPhoto"] == nil {
                 self.userImage.image = UIImage(named: "defaultPhoto")
             } else {
-                let decodedData = NSData(base64EncodedString: (self.currentUser["photoString"] as? String)!, options: NSDataBase64DecodingOptions())
+                let decodedData = NSData(base64EncodedString: (self.currentUser["userPhoto"] as? String)!, options: NSDataBase64DecodingOptions())
                 let decodedImage = UIImage(data: decodedData!)
                 self.userImage.image = decodedImage
             }
         })
+    }
+    
+    func getPhotos () {
+        FirebaseData.firebaseData.PHOTOS_REF.observeEventType(.Value, withBlock: { snapshots in
+            for snapshot in snapshots.children.allObjects as! [FDataSnapshot] {
+                if snapshot.value!["user"]! as! String == self.userDefaults.stringForKey("currentUser")! {
+                    let decodedData = NSData(base64EncodedString: (snapshot.value["photoString"] as? String)!, options: NSDataBase64DecodingOptions())
+                    let decodedImage = UIImage(data: decodedData!)
+                    self.userPhotosArray.insert(decodedImage!, atIndex: 0)
+                    print(self.userPhotosArray[0])
+                    self.tableView.reloadData()
+                    self.collectionView.reloadData()
+                }
+            }
+    })
     }
 
     //MARK: Custom Functions
