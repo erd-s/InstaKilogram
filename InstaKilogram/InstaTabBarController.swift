191ef580a6d3e4e0f8739640f6ec0dd708a6f2b9
Yemi Ajibola <ya9p8@mst.edu>
2016-02-09 10:05:04 -0600

    Switching to development branch.

--- InstaKilogram/InstaTabBarController.swift
+++ InstaKilogram/InstaTabBarController.swift
@@ -10,11 +10,26 @@ import UIKit
 import Firebase
 import Foundation
 
-var currentUser: String?
+//var currentUser: String?
 
 class InstaTabBarController: UITabBarController {
     
-    override func viewDidLoad() {
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+                FirebaseData.firebaseData.CURRENT_USER_REF.observeEventType(FEventType.Value, withBlock: { snapshot in
+            currentUser = snapshot.value.objectForKey("username") as? String
+            
+            
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
+
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         FirebaseData.firebaseData.CURRENT_USER_REF.observeEventType(FEventType.Value, withBlock: { snapshot in
             currentUser = snapshot.value.objectForKey("username") as? String
