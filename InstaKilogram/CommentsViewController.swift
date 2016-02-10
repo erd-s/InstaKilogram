b73072f6f2998b366d1429b0f9bf72dd94d01ba1
Jon Jones <jon.p.jones13@gmail.com>
2016-02-10 09:50:55 -0600

    Added comment view controller and getting ready to begin comments

---
+++ InstaKilogram/CommentsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CommentsViewController.swift
+//  InstaKilogram
+//
+//  Created by Jonathan Jones on 2/10/16.
+//  Copyright © 2016 mm. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class CommentsViewController: UIViewController {
+    
+    
+    
+    
+
+}
